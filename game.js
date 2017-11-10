cf207aac797a68ef90c0d16000fad688897f397c
Mateusz Biłgorajski <bilgorajskim@gmail.com>
2017-11-10 01:35:02 +0100

    Add .gitignore

--- game.js
+++ game.js
@@ -32,6 +32,8 @@ arrow.rotation = bow.rotation
 container.addChild(arrow);
 const mousePos = interactionManager.mouse.global
 arrow.target = new Victor(mousePos.x - bow.x, mousePos.y - bow.y);
+arrow.velocityX = 20
+arrow.velocityY = 0
 arrows.push(arrow)
   window.setTimeout(() => {
     container.removeChild(arrow)
@@ -43,12 +45,21 @@ app.ticker.add(function(delta) {
   const mouseVec = new Victor(mousePos.x - bow.x, mousePos.y - bow.y);
   bow.rotation = mouseVec.horizontalAngle()
   arrows.forEach(arrow => {
-    const targetVec = arrow.target.normalize().multiply(
-      new Victor(delta * 10, delta * 10)
+    const GRAVITY = 0.2
+    arrow.velocityY += delta * GRAVITY
+    arrow.velocityX -= delta * 0.1
+    const targetVec = arrow.target.clone().normalize().multiply(
+      new Victor(delta * arrow.velocityX, delta * arrow.velocityX)
     )
+    if (arrow.velocityX < 18) {
+      arrow.rotation -= Math.min(2, 0.01 * delta * (20 / arrow.velocityX))
+    }
     let arrowVector = new Victor(arrow.x, arrow.y);
-  let newVector = arrowVector.add(targetVec)
-  arrow.x = newVector.x
-  arrow.y = newVector.y
-})
+    let newVector = arrowVector.add(targetVec)
+      .add(new Victor(
+      0, arrow.velocityY
+    ))
+    arrow.x = newVector.x
+    arrow.y = newVector.y
+  })
 });
